66f7bc0447a4038fdce8f2d8fc96d2fc0feaecfd
svetozar12 <s.gospodinov02@gmail.com>
2023-09-16 16:23:49 +0300

    .

---
+++ libs/shared/sdk/src/index.ts
@@ -0,0 +1,21 @@
+import { Configuration, UserApi } from './api/v1';
+
+const API_URL = `${process.env['NEXT_PUBLIC_SCHEMA'] || 'http'}://${
+  process.env['NEXT_PUBLIC_HOST'] || 'localhost'
+}:${process.env['NEXT_PUBLIC_PORT'] || '3333'}`;
+const config = new Configuration({ basePath: API_URL });
+
+export const initSdk = (sdkConfig: Configuration) => {
+  return new Configuration(sdkConfig);
+};
+
+export const setToken = (token: string) => {
+  if (!token) return;
+  config.accessToken = token;
+};
+
+export const sdk = {
+  userInstance: () => new UserApi(config),
+};
+
+export * as API from './api/v1';
